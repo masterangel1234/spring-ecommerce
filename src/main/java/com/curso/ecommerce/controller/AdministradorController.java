0c58219c0a211f58696f44432b1423cf86bd6789
angel <angel1284@live.com.mx>
2021-12-03 17:29:14 -0600

    implementar la funcionalidades de caragra imagen y mostrarimagen para el administrador

--- src/main/java/com/curso/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecommerce/controller/AdministradorController.java
@@ -1,15 +1,29 @@
 package com.curso.ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.curso.ecommerce.model.Producto;
+import com.curso.ecommerce.service.ProdutoService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
 	
+	@Autowired
+	private ProdutoService productoService;
+	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		
+		List<Producto> productos = productoService.findAll();
+		model.addAttribute("productos", productos);
+		
 		return "administrador/home";
 	}
 
