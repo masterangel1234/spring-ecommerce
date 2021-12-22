e2ec22f90ab6e62282fd346f2129820053130512
angel <angel1284@live.com.mx>
2021-12-21 21:51:41 -0600

    mostramos la informacion del usuario y el de talle del carrito en la vista resumenorden

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.curso.ecommerce.model.DetalleOrden;
 import com.curso.ecommerce.model.Orden;
 import com.curso.ecommerce.model.Producto;
+import com.curso.ecommerce.model.Usuario;
+import com.curso.ecommerce.service.IUsuarioService;
 import com.curso.ecommerce.service.ProdutoService;
 
 @Controller
@@ -28,6 +30,9 @@ public class HomeController {
 	
 	@Autowired
 	private ProdutoService productoService;
+	
+	@Autowired
+	private IUsuarioService usuarioService;
 	
 	// para almacenar los detalles de la orden
 	List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
@@ -132,7 +137,14 @@ public class HomeController {
 	
 	@GetMapping("/order")
 	
-	public String order(){
+	public String order(Model model){
+		
+		Usuario usuario = usuarioService.findById(1).get();
+		
+		
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		model.addAttribute("usuario", usuario);
 		
 		return "usuario/resumenorden";
 	}
---
+++ src/main/java/com/curso/ecommerce/service/UsuarioServiceImpl.java
@@ -0,0 +1,26 @@
+package com.curso.ecommerce.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.curso.ecommerce.model.Usuario;
+import com.curso.ecommerce.repository.IUsuarioRepository;
+
+@Service
+public class UsuarioServiceImpl implements IUsuarioService {
+	
+	@Autowired
+	private IUsuarioRepository usuariorepository;
+
+	@Override
+	public Optional<Usuario> findById(Integer id) {
+		// TODO Auto-generated method stub
+		return usuariorepository.findById(id);
+	}
+
+	
+	
+
+}
