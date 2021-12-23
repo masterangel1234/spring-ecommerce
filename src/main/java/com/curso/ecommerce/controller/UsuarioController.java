6dffc6f1387c4a971844d70889cd220ab949c6dd
angel <angel1284@live.com.mx>
2021-12-23 00:20:49 -0600

    implementar la vista regisro  paa el suaurio y su controlador

---
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -0,0 +1,23 @@
+package com.curso.ecommerce.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.curso.ecommerce.service.IUsuarioService;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+	@Autowired
+	private IUsuarioService usuarioService;
+	
+	//usuario / registro
+	@GetMapping("/registro")
+	
+	public String create(){
+		return "usuario/registro";
+	}
+}
